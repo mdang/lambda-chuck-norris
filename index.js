11d0de53b0dd3c13441382cbad8fb638099ad240
Mike Dang <mike@packstack.co>
2016-09-21 15:46:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 'use strict';
 
+var https = require('https');
+
 console.log('Loading function');
 
 exports.handler = (event, context, callback) => {
     //console.log('Received event:', JSON.stringify(event, null, 2));
-    var response = {
-        text: 'Chuck Norris has already been to Mars; that\'s why there are no signs of life.'
-    }
+    
+    http.get({
+        host: 'api.icndb.com',
+        path: '/jokes/random?firstName=Chuck&amp;lastName=Norris'
+    }, function(response) {
+        // Continuously update stream with data
+        var body = '';
+        response.on('data', function(d) {
+            body += d;
+        });
+        response.on('end', function() {
+
+            // Data reception is done, do whatever with it!
+            var parsed = JSON.parse(body);
+            callback({
+                text: parsed.value.joke
+            });
+        });
+    });
+    
     callback(null, response);
 };
